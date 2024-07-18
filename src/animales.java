6ca10d38d8f2b15494e5a61b461d03471d000d66
HP <HP@DESKTOP-3E1G1FC>
2024-07-17 22:58:51 -0600

    mi avance

---
+++ src/animales.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author HP
+ */
+public class animales {
+ String tipodeanimal;
+    String raza;
+    double peso;
+    int edad;
+    String color;
+    int tipo;
+
+    // Constructor vacío
+    public animales() {
+    }
+
+    // Métodos getter y setter para cada atributo
+    public String getTipodeanimal() {
+        return tipodeanimal;
+    }
+
+    public void setTipodeanimal(String tipodeanimal) {
+        this.tipodeanimal = tipodeanimal;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+}
